6c0d6a5c3f578f7e56f1a0ee691bf9cc2735f9f5
neilshah2000 <neilshahlimited@hotmail.com>
2022-01-26 13:54:55 +0000

    cleaned up boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    MenuList,
+    MenuItem,
+    ListItemText,
+    Button,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    onClick={console.log}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Minidapp Boilerplate
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
-export default App;
+export default App
